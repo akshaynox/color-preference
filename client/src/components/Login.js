8fc551061a4400997df7f1ce5b316e7e91becf4b
Akshay <aksingh240@gmail.com>
2022-12-05 12:08:04 +0530

    fixed eslint issue blocking deployment

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
         if (localStorage.getItem('user')) {
             navigate("/dashboard");
         }
-    }, [])
+    }, [navigate])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
